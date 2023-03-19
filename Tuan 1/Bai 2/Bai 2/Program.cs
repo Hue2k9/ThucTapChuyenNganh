2e4a1d13fcd24becdea818da26cfe6496dfb0085
Hue2k9 <httt2hauik15@gmail.com>
2023-03-19 09:24:18 +0700

    add week 1 exercise

---
+++ Tuan 1/Bai 2/Bai 2/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Bai_2
+{
+    internal class Program
+    {
+        /*
+         * Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+          Return the running sum of nums.
+         */
+        static public int[] RunningSum(int[] nums)
+        {
+            int[] runningSum = new int[nums.Length];
+            int sum = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                sum += nums[i];
+                runningSum[i] = sum;
+            }
+            return runningSum;
+        }
+        static void Main(string[] args)
+        {
+            int[] nums = new int[] { 1, 2, 3, 4 };
+            int[] runningSum = RunningSum(nums);
+            for(int i=0; i<runningSum.Length; i++)
+            {
+                Console.Write(runningSum[i]+" ");
+            }
+        }
+    }
+}
---
+++ Tuan 1/bai 1/ConsoleApp2/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Nhap ho ten: ");
+            string hoTen = Console.ReadLine();
+
+            Console.Write("Nhap diem: ");
+            double diem = double.Parse(Console.ReadLine());
+
+            string xepLoai = "";
+            if (diem >= 8)
+            {
+                xepLoai = "GIOI";
+            }
+            else if (diem >= 6.5)
+            {
+                xepLoai = "KHA";
+            }
+            else if (diem >= 5)
+            {
+                xepLoai = "TRUNG BINH";
+            }
+            else
+            {
+                xepLoai = "YEU";
+            }
+
+            Console.WriteLine("Ho ten: " + hoTen.ToUpper());
+            Console.WriteLine("Xep loai: " + xepLoai);
+        }
+    }
+}
